50184cbfc95fb8f9ca93cb6227046b8dcece1489
Solomiia Leshchuk <solosun0308@gmail.com>
2026-03-26 00:19:26 +0200

    Update solution.py

--- Linked Lists - Get Nth Node/solution.py
+++ Linked Lists - Get Nth Node/solution.py
@@ -1,1 +1,17 @@
+from preloaded import Node
 
+def get_nth(node, index):
+    if not node or index < 0:
+        raise Exception("Invalid index value should throw error.")
+    
+    curr = node
+    counter = 0
+    
+    while curr:
+        if counter == index:
+            return curr
+        
+        curr = curr.next
+        counter += 1
+    
+    raise Exception("Index out of bounds")
