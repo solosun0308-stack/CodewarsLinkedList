ff11e06076e25e222a780c360f79bd4c7652d965
Solomiia Leshchuk <solosun0308@gmail.com>
2026-03-25 20:15:37 +0200

    Update solution.py

--- Convert-a-linked-list-to-a-string/solution.py
+++ Convert-a-linked-list-to-a-string/solution.py
@@ -1,1 +1,11 @@
-
+def stringify(node):
+    res = []
+    curr = node
+    
+    while curr:
+        res.append(str(curr.data))
+        curr = curr.next
+    
+    res.append("None")
+    
+    return " -> ".join(res)
