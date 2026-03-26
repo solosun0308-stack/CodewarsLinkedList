57c37d90d32ab8e67cf2a2075d96582d23b9044e
Solomiia Leshchuk <solosun0308@gmail.com>
2026-03-26 09:06:55 +0200

    Update solution.py

--- Linked Lists - Move Node/solution.py
+++ Linked Lists - Move Node/solution.py
@@ -1,1 +1,20 @@
-
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    if source is None:
+        raise Exception("Source list is empty")
+    
+    new_source = source.next
+    
+    source.next = dest
+    new_dest = source
+    
+    return Context(new_source, new_dest)
