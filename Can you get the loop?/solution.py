3248f2f59162a60e95dbac2c0e60efa124f0f7e9
Solomiia Leshchuk <solosun0308@gmail.com>
2026-03-27 17:25:35 +0200

    Update solution.py

--- Can you get the loop?/solution.py
+++ Can you get the loop?/solution.py
@@ -1,1 +1,15 @@
-
+def loop_size(node):
+    turtle = node.next
+    rabbit = node.next.next
+    
+    while turtle != rabbit:
+        turtle = turtle.next
+        rabbit = rabbit.next.next
+    
+    count = 1
+    rabbit = turtle.next
+    while turtle != rabbit:
+        rabbit = rabbit.next
+        count += 1
+        
+    return count
